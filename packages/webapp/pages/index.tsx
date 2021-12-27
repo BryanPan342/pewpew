ae7f85697b496f121240b2400abee5e1d710c4e8
Bryan Pan <bryanpan342@gmail.com>
2021-12-26 18:33:11 -0800

    feat: prelim chat app

--- packages/webapp/pages/index.tsx
+++ packages/webapp/pages/index.tsx
@@ -1,66 +1,41 @@
-import Head from 'next/head';
-import React from 'react';
+import Gun from 'gun';
+import React, {useEffect, useState} from 'react';
 import styles from '../styles/Home.module.scss';
 
+const gun = Gun({
+  peers: ['http://localhost:8000/gun'],
+});
+
 export default function Home(): JSX.Element {
+  const [txt, setTxt] = useState();
+
+  useEffect(() => {
+    // retrieve text on load
+    gun.get('text').once((node) => {
+      if(node == undefined) {
+        gun.get('text').put({text: 'Write the text here'});
+      } else {
+        setTxt(node.text);
+      }
+    });
+
+    // whenever the text loads, update state
+    gun.get('text').on((node) => {
+      setTxt(node.text);
+    });
+  }, []);
+
+  // Update local state and gun.db state
+  const updateText = (event) => {
+    gun.get('text').put({text: event.target.value});
+    setTxt(event.target.value);
+  };
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+    <div id={styles['text-container']}>
+      <h1>Collaborative Docs with Gun.js</h1>
+      <textarea id={styles.text}value = {txt} onChange = {updateText}/>
     </div>
+
   );
 }
