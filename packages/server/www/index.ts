17eee7adccde53e59aaaaf52187d8b91fc8d846f
Bryan Pan <bryanpan342@gmail.com>
2021-12-26 18:20:34 -0800

    feat: preliminary Gun.js relay server

--- packages/server/www/index.ts
+++ packages/server/www/index.ts
@@ -1,2 +1,13 @@
-const hello = 'hello world';
-hello;
+import http from 'http';
+import Gun from 'gun';
+
+const server = http.createServer();
+
+const PORT = process.env.PORT ?? 3000;
+
+const www = server.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log('Listening at: http://localhost:' + PORT);
+});
+
+Gun({web: www});
