22b94cd4c91503a8546eceeb0cc5967d10d3b1c6
Bryan Pan <bryanpan342@gmail.com>
2022-01-04 15:49:34 -0800

    chore: more testing

--- packages/server/www/index.ts
+++ packages/server/www/index.ts
@@ -5,16 +5,16 @@ const server = http.createServer();
 
 const PORT = process.env.OPENSHIFT_NODEJS_PORT || process.env.VCAP_APP_PORT || process.env.PORT || 8765;
 
-const www = server.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log('Listening at: http://localhost:' + PORT);
-});
-
 // eslint-disable-next-line no-console
 console.log({
   key: process.env.AWS_ACCESS_KEY_ID, // AWS Access Key
   secret: process.env.AWS_SECRET_ACCESS_KEY, // AWS Secret Token
   bucket: process.env.AWS_S3_BUCKET, // The bucket you want to save into
+});
+
+const www = server.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log('Listening at: http://localhost:' + PORT);
 });
 
 Gun({
