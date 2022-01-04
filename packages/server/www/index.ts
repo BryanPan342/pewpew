951fb54b95bd67e70a83a406fccf67eaec6e934d
Bryan Pan <bryanpan342@gmail.com>
2022-01-04 12:17:36 -0800

    chore: testing

--- packages/server/www/index.ts
+++ packages/server/www/index.ts
@@ -10,6 +10,13 @@ const www = server.listen(PORT, () => {
   console.log('Listening at: http://localhost:' + PORT);
 });
 
+// eslint-disable-next-line no-console
+console.log({
+  key: process.env.AWS_ACCESS_KEY_ID, // AWS Access Key
+  secret: process.env.AWS_SECRET_ACCESS_KEY, // AWS Secret Token
+  bucket: process.env.AWS_S3_BUCKET, // The bucket you want to save into
+});
+
 Gun({
   web: www,
   file: 'data.json',
